ce6a8ae63bcad96d826d2323667123807cdaad54
adrian.frimpong <adrian.frimpong@gmail.com>
2014-04-15 18:05:31 -0400

    db migrated to include the Posts model

--- app/models.py
+++ app/models.py
@@ -8,7 +8,22 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	nickname = db.Column(db.String(64), index = True, unique = True)
 	email = db.Column(db.String(120), index = True, unique = True)
-	role = db.Column(db.Integer, default = ROLE_USER) 
+	role = db.Column(db.Integer, default = ROLE_USER)
+	
+	# relationships
+	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
 	def __repr__(self):
-		return '<User {}>'.format(self.nickname)
+		return '<User %r>'% (self.nickname)
+
+class Post(db.Model):
+	"model for Posts table"
+	id = db.Column(db.Integer, primary_key = True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime)
+	# relationships
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+	def __repr__(self):
+		return '<Post %r>' % (self.body)
--- db_create.py
+++ db_create.py
@@ -3,7 +3,7 @@ from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
 import os.path
-db.create_all()
+db.create_all()	
 
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
 	api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
