861f7d49bc3e5a0916c92cf14daddd78a0292bec
adrian.frimpong <adrian.frimpong@gmail.com>
2014-05-07 14:55:11 -0400

    Added tests for avoiding identical nickname collision. Still need to add behave tests for BDD

---
+++ flask/Scripts/nosetests-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\afrimpong\PIP_Projects\microblog\flask\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'nose==1.3.3','console_scripts','nosetests'
+__requires__ = 'nose==1.3.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('nose==1.3.3', 'console_scripts', 'nosetests')()
+    )
