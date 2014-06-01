109dc5d079feb98d415dc871cc4a687832abd21d
adrian.frimpong <adrian.frimpong@gmail.com>
2014-06-01 17:23:27 -0400

    Users can add posts to their microblog

--- app/forms.py
+++ app/forms.py
@@ -31,3 +31,7 @@ class EditForm(Form):
                 'This nickname is already in use. Please choose another one.')
             return False
         return True
+
+
+class PostForm(Form):
+    post = TextField('post', validators=[Required()])
--- app/views.py
+++ app/views.py
@@ -1,32 +1,30 @@
 from flask import render_template, flash, redirect, session, url_for, request, g, make_response, jsonify, Response
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm
-from models import User, ROLE_USER, ROLE_ADMIN
+from forms import LoginForm, EditForm, PostForm
+from models import User, ROLE_USER, ROLE_ADMIN, Post
 from urllib2 import urlopen
 from json import loads
 from datetime import datetime
 
 
-@app.route('/')  # index view, default when going to
-@app.route('/index')
+@app.route('/', methods= ['GET', 'POST'])  # index view, default when going to
+@app.route('/index', methods= ['GET', 'POST'])
 @login_required
 def index():
-    user = g.user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body= form.post.data, timestamp = datetime.utcnow(), author = g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post is now live!")
+        return redirect(url_for('index'))
+    
+    posts = g.user.followed_posts().all()
 
     return render_template('index.html',
                            title='Home',
-                           user=user,
+                           form = form,
                            posts=posts)
 
 
